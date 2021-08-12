89174e434d976b55ec3ef40f139735ee20e32727
Dakota States <dakota.states@gmail.com>
2021-08-11 19:10:56 -0700

    Current User's Video style

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -92,7 +92,7 @@ const ContextProvider = ({ children }) =>{
   }
 
   return(
-    <SocketContext.Provider value={{ call, callAccpeted, myVideo, userVideo, stream, name, setName, callEnded, me, callUser, leaveCall, answerCall }}>
+    <SocketContext.Provider value={{ call, callAccepted, myVideo, userVideo, stream, name, setName, callEnded, me, callUser, leaveCall, answerCall }}>
       {children}
     </SocketContext.Provider>
   )
