bee51d43e290ef92c868969851bb0cac057ce007
Dakota States <dakota.states@gmail.com>
2021-08-11 20:35:05 -0700

    options component - Make a call and hangup functionality

--- client/src/components/Options.jsx
+++ client/src/components/Options.jsx
@@ -57,11 +57,39 @@ const Options = ( {children} ) =>{
                 </Button>
               </CopyToClipboard>
             </Grid>
+            <Grid item xs={12} md={6} className={classes.padding}>
+              <Typography gutterBottom variant="h6">Make A Call</Typography>
+              <TextField label="ID to Call" value={idToCall} onChange={(e)=> setIdToCall(e.target.value)} fullWidth/>
+              {callAccepted && !callEnded ? (
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  startIcon={<PhoneDisabled fontSize="large"/>}
+                  fullWidth
+                  onClick={leaveCall}
+                  className={classes.margin}
+                  >
+                  Hang Up
+                </Button>
+              ):
+              (
+                <Button
+                  variant="contained"
+                  color="primary"
+                  startIcon={<Phone fontSize="large"/>}
+                  fullWidth
+                  onClick={()=>callUser(idToCall)}
+                  className={classes.margin}
+                >
+                  Call
+                </Button>
+              )}
+            </Grid>
           </Grid>
         </form>
+        {children}
       </Paper>
-      options
-      {children}
+
     </Container>
   )
 }
